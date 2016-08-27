5a78ef645a5d137f42a1fe68eb4820e405d8a8f1
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-08-27 00:57:33 -0300

    Running rubocop

--- lib/json_formatter.rb
+++ lib/json_formatter.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 require 'json'
 
 class JSONFormatter < XCPretty::Simple
-  FILE_PATH = 'build/reports/errors.json'
+  FILE_PATH = 'build/reports/errors.json'.freeze
 
   def initialize(use_unicode, colorize)
     super
@@ -32,11 +32,11 @@ class JSONFormatter < XCPretty::Simple
 
   def format_compile_warning(file_name, file_path, reason, line, cursor)
     @compile_warnings << {
-      :file_name => file_name,
-      :file_path => file_path,
-      :reason => reason,
-      :line => line,
-      :cursor => cursor
+      file_name: file_name,
+      file_path: file_path,
+      reason: reason,
+      line: line,
+      cursor: cursor
     }
     write_to_file_if_needed
     super
@@ -50,11 +50,11 @@ class JSONFormatter < XCPretty::Simple
 
   def format_compile_error(file, file_path, reason, line, cursor)
     @compile_errors << {
-      :file_name => file,
-      :file_path => file_path,
-      :reason => reason,
-      :line => line,
-      :cursor => cursor
+      file_name: file,
+      file_path: file_path,
+      reason: reason,
+      line: line,
+      cursor: cursor
     }
     write_to_file_if_needed
     super
@@ -62,8 +62,8 @@ class JSONFormatter < XCPretty::Simple
 
   def format_file_missing_error(reason, file_path)
     @file_missing_errors << {
-      :file_path => file_path,
-      :reason => reason
+      file_path: file_path,
+      reason: reason
     }
     write_to_file_if_needed
     super
@@ -71,9 +71,9 @@ class JSONFormatter < XCPretty::Simple
 
   def format_undefined_symbols(message, symbol, reference)
     @undefined_symbols_errors = {
-      :message => message,
-      :symbol => symbol,
-      :reference => reference
+      message: message,
+      symbol: symbol,
+      reference: reference
     }
     write_to_file_if_needed
     super
@@ -81,8 +81,8 @@ class JSONFormatter < XCPretty::Simple
 
   def format_duplicate_symbols(message, file_paths)
     @duplicate_symbols_errors = {
-      :message => message,
-      :file_paths => file_paths,
+      message: message,
+      file_paths: file_paths
     }
     write_to_file_if_needed
     super
@@ -102,19 +102,19 @@ class JSONFormatter < XCPretty::Simple
 
   def json_output
     {
-      :warnings => @warnings,
-      :ld_warnings => @ld_warnings,
-      :compile_warnings => @compile_warnings,
-      :errors => @errors,
-      :compile_errors => @compile_errors,
-      :file_missing_errors => @file_missing_errors,
-      :undefined_symbols_errors => @undefined_symbols_errors,
-      :duplicate_symbols_errors => @duplicate_symbols_errors,
-      :tests_failures => @failures,
-      :tests_summary_messages => @tests_summary_messages
+      warnings: @warnings,
+      ld_warnings: @ld_warnings,
+      compile_warnings: @compile_warnings,
+      errors: @errors,
+      compile_errors: @compile_errors,
+      file_missing_errors: @file_missing_errors,
+      undefined_symbols_errors: @undefined_symbols_errors,
+      duplicate_symbols_errors: @duplicate_symbols_errors,
+      tests_failures: @failures,
+      tests_summary_messages: @tests_summary_messages
     }
   end
-  
+
   def write_to_file_if_needed
     write_to_file unless XCPretty::Formatter.method_defined? :finish
   end
@@ -122,11 +122,11 @@ class JSONFormatter < XCPretty::Simple
   def write_to_file
     file_name = ENV['XCPRETTY_JSON_FILE_OUTPUT'] || FILE_PATH
     dirname = File.dirname(file_name)
-    FileUtils::mkdir_p dirname
+    FileUtils.mkdir_p dirname
 
-    File.open(file_name, 'w') { |io|
+    File.open(file_name, 'w') do |io|
       io.write(JSON.pretty_generate(json_output))
-    }
+    end
   end
 end
 
--- xcpretty-json-formatter.gemspec
+++ xcpretty-json-formatter.gemspec
@@ -1,27 +1,28 @@
 # coding: utf-8
 
 Gem::Specification.new do |spec|
-  spec.name          = "xcpretty-json-formatter"
-  spec.version       = "0.0.2"
-  spec.authors       = ["Marcelo Fabri"]
-  spec.email         = ["me@marcelofabri.com"]
+  spec.name          = 'xcpretty-json-formatter'
+  spec.version       = '0.0.2'
+  spec.authors       = ['Marcelo Fabri']
+  spec.email         = ['me@marcelofabri.com']
 
-  spec.summary       = %q{xcpretty custom formatter for JSON output}
-  spec.description   = %q{Custom formatter for xcpretty that saves on a JSON file all the errors, warnings and test failures, so you can process them easily later.}
-  spec.homepage      = "https://github.com/marcelofabri/xcpretty-json-formatter"
-  spec.license       = "MIT"
+  spec.summary       = 'xcpretty custom formatter for JSON output'
+  spec.description   = 'Custom formatter for xcpretty that saves on a JSON file all the errors, warnings and test failures, so you can process them easily later.'
+  spec.homepage      = 'https://github.com/marcelofabri/xcpretty-json-formatter'
+  spec.license       = 'MIT'
 
   spec.files         = [
-  	"README.md",
-  	"LICENSE",
-  	"lib/json_formatter.rb",
-  	"bin/xcpretty-json-formatter"]
-  spec.executables   = ["xcpretty-json-formatter"]
-  spec.require_paths = ["lib"]
+    'README.md',
+    'LICENSE',
+    'lib/json_formatter.rb',
+    'bin/xcpretty-json-formatter'
+  ]
+  spec.executables   = ['xcpretty-json-formatter']
+  spec.require_paths = ['lib']
 
-  spec.add_dependency "xcpretty", "~> 0.2", ">= 0.0.7"
+  spec.add_dependency 'xcpretty', '~> 0.2', '>= 0.0.7'
 
-  spec.add_development_dependency "bundler", "~> 1.11"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bundler', '~> 1.11'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
 end
