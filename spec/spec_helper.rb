fd0c272d87d51c4cbde5489a322937996d6131e0
Marcelo Fabri <marcelofabrimf@gmail.com>
2016-08-27 00:57:51 -0300

    Adding tests

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+require 'pathname'
+ROOT = Pathname.new(File.expand_path('../../', __FILE__))
+$LOAD_PATH.unshift((ROOT + 'lib').to_s)
+$LOAD_PATH.unshift((ROOT + 'spec').to_s)
+
+require 'bundler/setup'
+
+require 'rspec'
+
+# Use coloured output, it's the best.
+RSpec.configure do |config|
+  config.filter_gems_from_backtrace 'bundler'
+  config.color = true
+  config.tty = true
+end
+
+def fixture(file)
+  File.read("spec/fixtures/#{file}")
+end
+
+def json_fixture(file, parsing = true)
+  content = fixture("#{file}.json")
+  content = JSON.parse(content) if parsing
+
+  content
+end
